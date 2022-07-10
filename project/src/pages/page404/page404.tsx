037f82fbe3fb6708ff123f516f40ea042d7fee19
miumio <nickolskaya@gmail.com>
2022-07-10 23:11:40 +0300

    feat: add pages

---
+++ project/src/pages/page404/page404.tsx
@@ -0,0 +1,8 @@
+export default function Page404(): JSX.Element {
+  return (
+    <section className="page404">
+      <p>404 - 404 Not Found</p>
+      <a href="main.html">main page</a>
+    </section>
+  )
+};
